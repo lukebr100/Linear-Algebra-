ba3e4738fb16b197a38640d339ae215a4385c356
Luke Ballantine Bennett <bennettluke33@yahoo.com>
2023-06-20 18:31:23 -0500

    Update gaussian_elim.py

--- gaussian_elim.py
+++ gaussian_elim.py
@@ -5,66 +5,101 @@ from print_matrix import print_matrix
 from rowops import rowswap
 from row_i import row
 
-
-def gaussian_elim(A):
-    if  vector_matrix_test(A)[0] == True and detect_type(A)[0] == True:
-        m = len(A)          # store number of sublists in A (rows)
-        n = len(A[0])       # store the number of columns of A
-        C = [ [0 for _ in range(n)] for _ in range(m) ]      # initailizes matrix of cofactors[sic?]
+def gaussian_elim(A, b):
+    m = len(A)
+    # store number of rows
+    if  vector_matrix_test(A)[0] == True and detect_type(A)[0] == True and len(b) == m:
+        n = len(A[0])
+        # store the number of columns of A
+        C = [ [0 for _ in range(n)] for _ in range(m) ]
+        # initailizes matrix of cofactors[sic?]
         B = [row(A, i) for i in range(m)]
-        for i in range(n):              # arbitrary column       
-            k = 0                       # counter
+        for i in range(n):
+            # arbitrary column       
+            k = 0
+            # counter
             
-            for j in range(i, m - 1):   # row from i, i + 1, ..., m - 2
+            for j in range(i, m - 1):
+                # row from i, i + 1, ..., m - 2
                 
-                if B[j - k][i] != 0 and B[j + 1][i] != 0:    # the pivot position is non-zero // j - k is constant in this loop   
-                    t = B[j - k][i]     # store pivot
-                    C[j + 1][i] = - (B[j + 1][i] / t)               # calculate factor
-                    B = rowadd( B, j - k, j + 1, C[j + 1][i])       # perform row op
-                    k = k + 1                                       # add to counter
-                elif B[j - k][i] == 0 and B[j + 1][i] != 0:         #  handles if a pivot position is zero, swaps zero in pivot spot w/ a nonzero entry  
-                    B = rowswap(B, j - k, j + 1)                    # rowswap of rows j - k and j + 1 in B
-                    return gaussian_elim(B)                         # run this algorithm on the result, the new element in the pivot is non-zero!
-        return B
+                if B[j - k][i] != 0 and B[j + 1][i] != 0:
+                    # the pivot position is non-zero // j - k is constant in this loop   
+                    t = B[j - k][i]
+                    # store pivot
+                    C[j + 1][i] = - (B[j + 1][i] / t)
+                    # calculate factor
+                    b[j + 1] = C[j + 1][i] * b[j - k] + b[j + 1]
+                    B = rowadd( B, j - k, j + 1, C[j + 1][i])
+                    # perform row op
+                    k = k + 1
+                    # add to counter
+                elif B[j - k][i] == 0 and B[j + 1][i] != 0:
+                    #  handles if a pivot position is zero, swaps zero in pivot spot w/ a nonzero entry  
+                    B = rowswap(B, j - k, j + 1)
+                    s = b[j - k]
+                    b[j - k] = b[j + 1]
+                    b[j + 1] = s
+                    # rowswap of rows j - k and j + 1 in B
+                    return gaussian_elim(B, b)
+                    # run this algorithm on the result, the new element in the pivot is non-zero!
+        return B, b
     else:
         return False
-    
 
-    
+def ge_incon_ID(A, b):
+    tildeA = gaussian_elim(A, b)
+    # call GE function, output REF version of A, corresponding b
+    n = len(A[0])
+    # number of columns
+    m = len(A[:])
+    # number of rows 
+    Zrow = [j for j in range(m) if tildeA[0][j] == [0 for i in range(n)]]
+    # identifies rows of zeros in tildeA[0](REF of A), return indexes where these occur
+    for k in Zrow:
+        # for all zero rows
+        if b[k] != 0:
+            # b at that index is non-zero
+            # but LHS is zero, ie, 0*x + 0*y + 0*z = 2
+            # inconsistent, return GE of Ax = b
+            return 'inconsistent', tildeA
+    return 'consistent', tildeA
+    # consistent system, return GE of Ax = b
 
-
-A = [[1, 2, 3],                               #works, result in REF
+A = [[1, 2, 3],
     [4, 5, 6], 
     [7, 8, 9]]
-# tildeA = gaussian_elim( A )
-# print_matrix(tildeA)
+b = [10, 11, 12]
+tildeA = ge_incon_ID( A, b )
+print(tildeA[0])
+print_matrix(tildeA[1])
 
-B = [[1, 2, 3, 4],                           # works
+B = [[1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 10, 11, 12]]
 # tildeB = (gaussian_elim( B ))
 # print_matrix(tildeB)
 
-C = [[1, 2, 3], [4, 5, 6], [7,8, 9], [10, 11, 12]]      # works
+C = [[1, 2, 3], [4, 5, 6], [7,8, 9], [10, 11, 12]]
 # tildeC = gaussian_elim( C )
 # print_matrix(tildeC)
 
-D = [[1, 1, 0], [1, 0, 1], [0, 1, 1]]           # works
+D = [[1, 1, 0], [1, 0, 1], [0, 1, 1]]
 # tildeD = gaussian_elim( D )
 # print_matrix(tildeD)
 
-E = [[0, 0, 0], [1, 1, 1], [1, 2, 3]]           # matrix NOT in REF before 6/16/23
-tildeE = gaussian_elim( E )             # WORKS, in REF! as of 6/16/23 this is due to handling the one elif: conditon using recursion  
-print_matrix(E)
-print_matrix(tildeE)       
+E = [[0, 0, 0], [1, 1, 1], [1, 2, 3]]
+b = [4, 5, 2]
+tildeE = ge_incon_ID( E, b )
+print(tildeE[0])   
+print_matrix(tildeE[1])
            
-E1 = [[1, 2, 3], [1, 1, 1], [0, 0, 0]]          # works
-tildeE1 = gaussian_elim( E1 )
-# print_matrix(tildeE1)
+E1 = [[1, 2, 3], [1, 1, 1], [0, 0, 0]]
+tildeE1 = ge_incon_ID( E1 , b)
+print(tildeE1[0])
+print_matrix(tildeE1[1])
 
 E2 = [[1, 2, 3], [0, 1, 0], [1, 0, 1]]
-# print_matrix(E2)                            # prints E2
-# tildeE2 = gaussian_elim( E2 )               # WORKS, in REF!
-# print_matrix(E2)                            # ??? doesn't print E2 // prints tildeE2 ??? what gives? tried correct code w/ adding dummy var B = A line
-# print_matrix(tildeE2)                       # same as print_matrix(E2)
-                                            # the above question was answered by adding B = A[:] similarly to row from row_i.py, rowscale_1 from rowops.py! 
+# print_matrix(E2)
+# tildeE2 = gaussian_elim( E2 )
+# print_matrix(E2)
+# # print_matrix(tildeE2)
